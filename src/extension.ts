3a3ec0813b2a73de424165bc48dfe84b69f8a8cf
Dhananjay-JSR <47073516+Dhananjay-JSR@users.noreply.github.com>
2022-03-04 23:23:56 +0530

    Added CPU Support

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,18 @@
 import * as vscode from 'vscode';
 import si = require('systeminformation');
+import os = require('os');
+const { exec } = require('child_process');
 let intervalIdSpeed:NodeJS.Timeout;
 let intervalIdCpuRam:NodeJS.Timeout;
 let statusBarSpeed:vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right,100);	//assigning statusbar function to a variable for a separate init
 let statusBarCpuRam:vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left,100);	
 let enabledSpeed:boolean = true;
 let enabledCPURam:boolean = true;
+let osVersion:string;
+let ramUsage:string;
+let cpuUsage:string;
 export function activate(context: vscode.ExtensionContext) {
+	osVersion = os.platform();
 	let disposable = vscode.commands.registerCommand('extension.show_hide_speed',()=>{
 		enabledSpeed = !enabledSpeed;
 		if (enabledSpeed===false){
@@ -58,10 +64,29 @@ function updateStatusBarSpeed(): void {
 	}, 1000);
 }
 function updateStatusBarCpuRam(): void {
+	osVersion = os.platform();
 	intervalIdCpuRam = setInterval(()=>{
 		si.mem().then(memdata => {
-			statusBarCpuRam.text=`RAM Usage ${(((memdata.used/memdata.total)*100)).toFixed(2).toString()} %`;
-			statusBarCpuRam.color="yellow";
+			ramUsage=(((memdata.used/memdata.total)*100)).toFixed(2);
+			if (osVersion==="win32"){
+				exec('wmic cpu get loadpercentage',(err:string,stdout:string,stderr:string)=>{
+					if (err){
+						cpuUsage=`Error Code 1 Please Open Issue`;
+					  return;
+					}
+					if (stderr){
+						cpuUsage=`Error Code 2 Please Open Issue`;
+					  return ;
+					}
+					cpuUsage = stdout.substring(15);
+					cpuUsage=cpuUsage.replace(/\s/g, '');
+					statusBarCpuRam.text=`RAM Usage :- ${ramUsage}% | CPU Usage:- ${cpuUsage} %`;
+					statusBarCpuRam.color="yellow";
+				  });
+			}
+			else{
+				cpuUsage=`Still work in Progress for ${osVersion}`;
+			}
 		});
 	}, 1000);
 }
